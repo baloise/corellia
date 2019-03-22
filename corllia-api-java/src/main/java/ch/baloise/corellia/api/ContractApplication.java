7ba2690f01f475f4e91190ce91f51d994e005ab7
Loris Sauter <loris.sauter@baloise.ch>
2019-03-22 16:53:43 +0100

    JAVAMIG-3479 Working on linking JsonPropertyDescription in auto-generated OpenAPI specs. This is PoC style code

--- corllia-api-java/src/main/java/ch/baloise/corellia/api/ContractApplication.java
+++ corllia-api-java/src/main/java/ch/baloise/corellia/api/ContractApplication.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
+import io.swagger.v3.oas.models.OpenAPI;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
@@ -15,4 +16,6 @@ public class ContractApplication extends Application {
       .registerModule(new ParameterNamesModule()) //
       .registerModule(new Jdk8Module()) //
       .registerModule(new JavaTimeModule()); // new module, NOT JSR310Module
+
+
 }
