d8a00ab091d51a527580b0cbdbfcb12acbc015ab
Markus Tiede <markus.tiede@baloise.ch>
2019-06-05 12:31:38 +0000

    Fix sonarqube finding: hide (default) constructor for constant class

--- src/main/java/ch/baloise/corellia/api/constraints/SizeConstraint.java
+++ src/main/java/ch/baloise/corellia/api/constraints/SizeConstraint.java
@@ -16,37 +16,40 @@
 package ch.baloise.corellia.api.constraints;
 
 public class SizeConstraint {
-  public final static int FIRST_NAME_MIN_SIZE = 2;
-  public final static int FIRST_NAME_MAX_SIZE  = 35;
-  public final static int LAST_NAME_MIN_SIZE = 2;
-  public final static int LAST_NAME_MAX_SIZE = 35;
+    public final static int FIRST_NAME_MIN_SIZE = 2;
+    public final static int FIRST_NAME_MAX_SIZE = 35;
+    public final static int LAST_NAME_MIN_SIZE = 2;
+    public final static int LAST_NAME_MAX_SIZE = 35;
 
-  public final static int EMAIL_MIN_SIZE = 2;
-  public final static int EMAIL_MAX_SIZE = 70;
-  public final static int PHONE_NUMBER_MIN_SIZE = 5;
-  public final static int PHONE_NUMBER_MAX_SIZE = 20;
+    public final static int EMAIL_MIN_SIZE = 2;
+    public final static int EMAIL_MAX_SIZE = 70;
+    public final static int PHONE_NUMBER_MIN_SIZE = 5;
+    public final static int PHONE_NUMBER_MAX_SIZE = 20;
 
-  public final static int LANGUAGE_SIZE = 2;
+    public final static int LANGUAGE_SIZE = 2;
 
-  public final static int COMPANY_NAME_MIN_SIZE = 3;
-  public final static int COMPANY_NAME_MAX_SIZE = 70;
-  public final static int UID_NUMBER_MAX_SIZE = 15;
+    public final static int COMPANY_NAME_MIN_SIZE = 3;
+    public final static int COMPANY_NAME_MAX_SIZE = 70;
+    public final static int UID_NUMBER_MAX_SIZE = 15;
 
-  public final static int AGENT_NUMBER_MAX_SIZE = 8;
+    public final static int AGENT_NUMBER_MAX_SIZE = 8;
 
-  public final static int STREET_MAX_SIZE = 35;
-  public final static int HOUSE_NUMBER_MAX_SIZE = 10;
-  public final static int ZIP_CODE_SIZE = 4;
-  public final static int CITY_MAX_SIZE = 10;
+    public final static int STREET_MAX_SIZE = 35;
+    public final static int HOUSE_NUMBER_MAX_SIZE = 10;
+    public final static int ZIP_CODE_SIZE = 4;
+    public final static int CITY_MAX_SIZE = 10;
 
-  public final static int LIST_MAX_SIZE = 99;
-  public final static int MONTH_YEAR_SIZE = 4;
-  public final static int FILE_NAME_MAX_SIZE = 256;
-  public final static int MESSAGE_MIN_SIZE = 1;
-  public final static int MESSAGE_MAX_SIZE = 1024;
+    public final static int LIST_MAX_SIZE = 99;
+    public final static int MONTH_YEAR_SIZE = 4;
+    public final static int FILE_NAME_MAX_SIZE = 256;
+    public final static int MESSAGE_MIN_SIZE = 1;
+    public final static int MESSAGE_MAX_SIZE = 1024;
 
-  public final static int CONTRACT_ID_MAX_SIZE = 20;
+    public final static int CONTRACT_ID_MAX_SIZE = 20;
 
-  public final static int CONTRACT_REFERENCE_MIN_SIZE = 1;
-  public final static int CONTRACT_REFERENCE_MAX_SIZE = 30;
+    public final static int CONTRACT_REFERENCE_MIN_SIZE = 1;
+    public final static int CONTRACT_REFERENCE_MAX_SIZE = 30;
+
+    private SizeConstraint() {
+    }
 }
