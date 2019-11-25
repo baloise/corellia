07e482ef94480954e6d130fd051ec524d259cc0c
David Weber <david.weber@baloise.ch>
2019-11-25 09:11:29 +0100

    Change identifier type from string to enum

--- src/main/java/ch/baloise/corellia/api/entities/Identifier.java
+++ src/main/java/ch/baloise/corellia/api/entities/Identifier.java
@@ -1,9 +1,29 @@
 package ch.baloise.corellia.api.entities;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonPropertyDescription;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import javax.validation.constraints.NotNull;
+import org.apache.commons.lang3.StringUtils;
 
 public class Identifier {
+
+  public enum IdentifierType {
+    INT, EXT;
+
+    private static Map<String, Identifier.IdentifierType> ELEMENTS_MAP = Stream.of(Identifier.IdentifierType.values()).collect(
+        Collectors.toMap(Enum::name, Function.identity()));
+
+    @JsonCreator // This is the factory method and must be static
+    public static Identifier.IdentifierType fromString(String string) {
+      final String upperCaseString = StringUtils.isEmpty(string) ? "" : string.toUpperCase();
+      return Optional.ofNullable(ELEMENTS_MAP.get(upperCaseString)).orElseThrow(() -> new IllegalArgumentException("cannot convert to Enumtype: " + string));
+    }
+  }
 
   @NotNull
   @JsonPropertyDescription("Id depending on the type.")
@@ -11,7 +31,7 @@ public class Identifier {
 
   @NotNull
   @JsonPropertyDescription("The type of the id. EXT or INT.")
-  private String type;
+  private IdentifierType type;
 
   public Identifier() {
   }
@@ -24,11 +44,11 @@ public class Identifier {
     this.id = id;
   }
 
-  public String getType() {
+  public IdentifierType getType() {
     return type;
   }
 
-  public void setType(String type) {
+  public void setType(IdentifierType type) {
     this.type = type;
   }
 }
