b1027377f37c253c252c73afca7e1f9786debc2b
David Weber <david.weber@baloise.ch>
2019-11-28 10:17:20 +0100

    Remove converter

--- src/main/java/ch/baloise/corellia/api/doc/OpenApi3DocGenerator.java
+++ src/main/java/ch/baloise/corellia/api/doc/OpenApi3DocGenerator.java
@@ -1,11 +1,9 @@
 package ch.baloise.corellia.api.doc;
 
-import io.swagger.v3.core.converter.ModelConverters;
 import io.swagger.v3.core.util.Json;
 import io.swagger.v3.jaxrs2.integration.JaxrsOpenApiContextBuilder;
 import io.swagger.v3.oas.integration.OpenApiConfigurationException;
 import io.swagger.v3.oas.models.OpenAPI;
-
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -25,7 +23,7 @@ public class OpenApi3DocGenerator {
     public static void main(String[] args) {
         try (FileWriter fw = new FileWriter("docs/openapi.json")){
             // Required to get the @JsonPropertyDescription
-            ModelConverters.getInstance().addConverter(new JsonPropertyDescriptionResolver(Json.mapper()));
+            // ModelConverters.getInstance().addConverter(new JsonPropertyDescriptionResolver(Json.mapper()));
 
             OpenAPI openAPI = new JaxrsOpenApiContextBuilder()
                     .buildContext(true)
