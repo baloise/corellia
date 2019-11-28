f4ec61342afd1e29c046f670b89a7f3701f62169
David Weber <david.weber@baloise.ch>
2019-11-28 10:22:55 +0100

    Revert "Remove resolver"
    This reverts commit ff6bbbbf53427cb7f441259ed78a940dac6426c9.

---
+++ src/main/java/ch/baloise/corellia/api/doc/JsonPropertyDescriptionResolver.java
@@ -0,0 +1,35 @@
+package ch.baloise.corellia.api.doc;
+
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.introspect.Annotated;
+import io.swagger.v3.core.jackson.ModelResolver;
+
+import java.lang.annotation.Annotation;
+
+
+/**
+ * Custom ModelResolver which resolves descriptions given by
+ * {@link JsonPropertyDescription} annotation.
+ * <br>
+ * The current description of the @{@link io.swagger.v3.oas.models.media.Schema} will always be overwritten
+ * by the {@link JsonPropertyDescription}'s value.
+ */
+public class JsonPropertyDescriptionResolver extends ModelResolver {
+    public JsonPropertyDescriptionResolver(ObjectMapper mapper) {
+        super(mapper);
+    }
+
+    @Override
+    protected String resolveDescription(Annotated a, Annotation[] annotations, io.swagger.v3.oas.annotations.media.Schema schema) {
+        if (annotations != null) {
+            for (Annotation ann : annotations) {
+                if (ann instanceof JsonPropertyDescription) {
+                    JsonPropertyDescription spd = (JsonPropertyDescription) ann;
+                    return spd.value();
+                }
+            }
+        }
+        return super.resolveDescription(a, annotations, schema);
+    }
+}
